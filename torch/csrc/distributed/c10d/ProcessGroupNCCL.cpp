b62144215f60ac3a063714d589c8fc9caa95c741
David Berard <dberard@fb.com>
2023-09-29 21:15:30 +0000

    [WIP] Remove recordStream for callback that ends a profiler event
    TODO - description:
    * The ProcessGroupNCCL callback that ends the profiler event
    * The future callback handling
    * calls synchronizeWithCurrentStreams()
    * that calls recordStream
    
    why this is WIP: I don't actually know why we need the synchronizeWithCurrentStreams()? I think we might actually be able to remove that sync all the time.
    
    ghstack-source-id: ce839556d471a2bb739ccfb6aaa5b5ae15058d60
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/109933

--- torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
+++ torch/csrc/distributed/c10d/ProcessGroupNCCL.cpp
@@ -1819,9 +1819,11 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::collective(
     // future blocks the stream this callback runs on the corresponding
     // ncclEndEvents_ ensuring appropriate synchronization.
     if (work->recordFunctionEndCallback_) {
-      work->future_->addCallback([work](at::ivalue::Future& /* unused */) {
-        work->recordFunctionEndCallback_();
-      });
+      work->future_->addCallback(
+          [work](at::ivalue::Future& /* unused */) {
+            work->recordFunctionEndCallback_();
+          },
+          /*uses_future=*/false);
     }
     work->future_->markCompleted(at::IValue(*work->outputs_));
   }
@@ -1999,9 +2001,11 @@ c10::intrusive_ptr<Work> ProcessGroupNCCL::pointToPoint(
   // future blocks the stream this callback runs on the corresponding
   // ncclEndEvents_ ensuring appropriate synchronization.
   if (work->recordFunctionEndCallback_) {
-    work->future_->addCallback([work](at::ivalue::Future& /* unused */) {
-      work->recordFunctionEndCallback_();
-    });
+    work->future_->addCallback(
+        [work](at::ivalue::Future& /* unused */) {
+          work->recordFunctionEndCallback_();
+        },
+        /*uses_future=*/false);
   }
 
   return work;
